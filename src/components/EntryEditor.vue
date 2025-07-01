08d7e3e17b10b5f34e55c2af921667f40ff3e02c
Al Zaplin <zaplin.dev@gmail.com>
2025-07-01 13:22:17 +0300

    Add:(code & notes) For Lesson #08: Writing type for computed-field on character counter feat. example

--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -1,11 +1,16 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import Emoji from "@/types/Emojis";
 import EmojiField from "@/components/EmojiField.vue";
 import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
 
 const text = ref("");
 const emoji = ref<Emoji | null>(null);
+
+// * 3.0 Нам нужно отображать количество введённых пользователем знаков в сообщение, т.к. мы хотим ограничить его 280 символами. Мы могли бы добиться этого же напрямую через text.length, т.к. это реактивная реф-переменная, но для примера типизации computed-полей сделаем это через такое поле.
+// 3.1.0 Если навести на "charCount", то мы увидим, что TS уже знает, что тип number, основываясь на результате, которое выдаёт это computed-поле. Поэтому совсем не обязательно указывать таким полям тип, ведь они уже работают с известными типами данных. Однако, мы всё же можем записать "computed<number>", если хотим проверить себя, что мы правильно напишем функцию, например, не забыв указать "length" после "value", с указанным явно типом TS будет в этом случае ругаться.
+// ? 3.1.1 Возможно конкретный пример не так показателен относительно пользы такого подхода, но бывают более сложные примеры с двумя и более состояниями, тогда это может быть весьма полезным. ↓
+const charCount = computed(() => text.value.length);
 </script>
 <template>
   <form class="entry-form" @submit.prevent>
@@ -15,7 +20,8 @@ const emoji = ref<Emoji | null>(null);
     ></textarea>
     <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>0 / 280</span>
+      <!-- 3.2 А теперь всё таки укажем вместо хардкода наше computed-поле. -->
+      <span>{{ charCount }} / 280</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
     </div>
   </form>
